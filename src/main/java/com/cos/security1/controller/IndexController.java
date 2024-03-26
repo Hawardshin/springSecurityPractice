ce6eebd3b099b913803fbc06f5b50c6b9d418e7c
hawardShin <joushin@student.42seoul.kr>
2024-03-26 17:49:48 +0900

    Feat: Annotation Secured, PreAuthorize

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.cos.security1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -63,4 +65,16 @@ public class IndexController {
 		return "redirect:/loginForm";
 	}
 
+	@Secured("ROLE_ADMIN")
+	@GetMapping("/info")
+	public @ResponseBody String info() {
+		return "개인정보";
+	}
+
+	@PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+	// @PostAuthorize() //이건 메서드가 끝난 후에 실행됨 잘 안 씁니다.
+	@GetMapping("/data")
+	public @ResponseBody String data() {
+		return "데이터 정보";
+	}
 }
