771210e438f84dba3c54481434f5e806d7e68869
hawardShin <joushin@student.42seoul.kr>
2024-03-26 16:45:57 +0900

    Feat: User Repository set query

--- src/main/java/com/cos/security1/repository/UserRepository.java
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -6,5 +6,12 @@ import com.cos.security1.model.User;
 
 //@Repository 없어도 Ioc가 자동으로 됩니다.
 public interface UserRepository extends JpaRepository<User, Integer>{
+	//findBy 규칙-> Username 문법
+	//select * from user where username = 1?;
+	public User findByUsername(String username);
+
+
+	//select * from user where email = 1?;
+	public User findByEmail(String email);
 
 }
