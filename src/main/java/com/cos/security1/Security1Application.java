03b72f943c52d58c8a132d867bb71c1febf9645d
hawardShin <joushin@student.42seoul.kr>
2024-04-06 13:50:04 +0900

    Feat: LoginGoogleForm

--- src/main/java/com/cos/security1/Security1Application.java
+++ src/main/java/com/cos/security1/Security1Application.java
@@ -2,12 +2,13 @@ package com.cos.security1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:googleAuth.properties")
 public class Security1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Security1Application.class, args);
 	}
-
 }
