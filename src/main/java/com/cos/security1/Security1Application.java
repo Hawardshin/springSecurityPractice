af5f215be00cfc30b71e7beaaafb46dad9afc5f4
hawardShin <joushin@student.42seoul.kr>
2024-04-08 20:30:09 +0900

    chore: rename property file

--- src/main/java/com/cos/security1/Security1Application.java
+++ src/main/java/com/cos/security1/Security1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:googleAuth.properties")
+@PropertySource("classpath:auth.properties")
 public class Security1Application {
 
 	public static void main(String[] args) {
