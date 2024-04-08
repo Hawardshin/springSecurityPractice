eb7e0d87ee727931d129f5dab6ae4795a31c1084
hawardShin <joushin@student.42seoul.kr>
2024-04-08 16:29:52 +0900

    Feat: User Entity Builder

--- src/main/java/com/cos/security1/model/User.java
+++ src/main/java/com/cos/security1/model/User.java
@@ -8,11 +8,28 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class User {
+
+	@Builder
+	public User(int id, String username, String password, String email, String role, String provider, String providerId,
+		Timestamp createDate) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.role = role;
+		this.provider = provider;
+		this.providerId = providerId;
+		this.createDate = createDate;
+	}
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id; //PK
@@ -20,6 +37,9 @@ public class User {
 	private String password;
 	private String email;
 	private String role; //ROLE_USER, ROLE_ADMIN
+
+	private String provider;
+	private String providerId;
 
 	@CreationTimestamp
 	private Timestamp createDate;
