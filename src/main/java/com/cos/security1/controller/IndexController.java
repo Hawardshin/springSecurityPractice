bc7e088c0b80c86ee639751421ca741a5e32e817
hawardShin <joushin@student.42seoul.kr>
2024-04-08 16:29:12 +0900

    Feat: check Security session and DI @AuthenticationPrincipal

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,12 +3,17 @@ package com.cos.security1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 
@@ -21,6 +26,29 @@ public class IndexController {
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+	@GetMapping("/test/login")
+	//AuthenticationPrincipal 어노테이션을 사용하면 세션 정보를 바로 받아올 수 있다.
+	public @ResponseBody String testLogin(Authentication authentication, @AuthenticationPrincipal PrincipalDetails userDetails){ //DI(의존성 주입)
+		System.out.println("/test/login =============");
+		PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+		System.out.println("authentication = " + principalDetails.toString());
+		System.out.println("authentication = " + principalDetails.getUsername());
+		System.out.println("userDetails = " + userDetails.getUsername());
+		return "세션 정보 확인하기";
+	}
+
+	@GetMapping("/test/oauth/login")
+	//AuthenticationPrincipal 어노테이션을 사용하면 세션 정보를 바로 받아올 수 있다.
+	// 이런식으로 두개 분기할 필요 없이 아래쪽에 하나의 객체를 사용하면 통합 가능
+	public @ResponseBody String testOAuthLogin(Authentication authentication,
+		@AuthenticationPrincipal OAuth2User oAuth){ //DI(의존성 주입)
+		System.out.println("/test/oauth/login =============");
+		OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+		System.out.println("authentication = " + oAuth2User.getAttributes());
+		System.out.println("oAuth2User = " + oAuth.getAttributes());
+		// super.loadUser(userRequest).getAttributes());
+		return "OAuth 세션 정보 확인하기";
+	}
 	@GetMapping({"","/"})
 	public String index(){
 		//머스테치 기본폴더 src/main/resources/
@@ -28,8 +56,10 @@ public class IndexController {
 		return "index"; //src/main/resources/templates/index.mustache
 	}
 
+	//통합됐다.
 	@GetMapping("/user")
-	public @ResponseBody String user() {
+	public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+		System.out.println("principalDetails = " + principalDetails.getUser());
 		return "user";
 	}
 
