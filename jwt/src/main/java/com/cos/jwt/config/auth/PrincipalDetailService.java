ddd690283a530f65cd0fa6c1e96e10c233b5af07
hawardShin <joushin@student.42seoul.kr>
2024-04-09 21:03:33 +0900

    Feat: PrincipalDetails and PrincipalDetailService

---
+++ jwt/src/main/java/com/cos/jwt/config/auth/PrincipalDetailService.java
@@ -0,0 +1,29 @@
+package com.cos.jwt.config.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+//http://localhost:8080/login 요청이 오면 자동으로 UserDetailsService 타입으로 IoC되어 있는 loadUserByUsername 함수가 실행
+//spring security가 기본적으로 요청하는 위치가 /login이기 때문이다.
+// http.formLogin(FormLoginConfigurer::disable); // form 로그인 비활성화
+// 이것 때문에 /login이 작동하지 않는다.
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailService implements UserDetailsService{
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		System.out.println("PrincipalDetailService의 loadUserByUsername");
+		User userEntity = userRepository.findByUsername(username);
+		return new PrincipalDetails(userEntity);
+	}
+}
