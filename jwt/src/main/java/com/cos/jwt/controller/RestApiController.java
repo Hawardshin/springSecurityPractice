0f606dd6ea4c422bd2789549319c7645c5e82fd0
hawardShin <joushin@student.42seoul.kr>
2024-04-09 21:04:00 +0900

    Feat: Add Logic for Set User

--- jwt/src/main/java/com/cos/jwt/controller/RestApiController.java
+++ jwt/src/main/java/com/cos/jwt/controller/RestApiController.java
@@ -1,14 +1,26 @@
 package com.cos.jwt.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 // @CrossOrigin // 이 어노테이션을 사용하면 모든 요청에 대해 CORS를 허용하게 될 수 있는데 이건 spring security의 인증이 필요한 모든 요청이 거부되기 때문에 사용하지 않는다.
 // 즉 인증이 필요 없는 요청만 보이게 됩니다.
 @RestController
+@RequiredArgsConstructor
 public class RestApiController {
+
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final UserRepository userRepository;
+
 	@GetMapping("/home")
 	public String home() {
 		return "<h1>home</h1>";
@@ -17,5 +29,13 @@ public class RestApiController {
 	@PostMapping("/token")
 	public String token() {
 		return "<h1>token</h1>";
+	}
+
+	@PostMapping("/join")
+	public String join(@RequestBody User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setRoles("ROLE_USER");
+		userRepository.save(user);
+		return "회원가입 완료";
 	}
 }
