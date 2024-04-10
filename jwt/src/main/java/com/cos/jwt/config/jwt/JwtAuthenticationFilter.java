34485095d6d53ac04de52f6881d4cff99017cb3a
hawardShin <joushin@student.42seoul.kr>
2024-04-10 13:55:08 +0900

    Feat: JwtAuthenticationFilter When do login -

--- jwt/src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
+++ jwt/src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -1,10 +1,23 @@
 package com.cos.jwt.config.jwt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.cos.jwt.config.auth.PrincipalDetails;
+import com.cos.jwt.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -35,6 +48,60 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws
 		AuthenticationException {
 		System.out.println("JwtAuthenticationFilter 로그인 시도중");
-		return super.attemptAuthentication(request, response);
+
+		// 1. username, password 받아서
+
+		//리퀘스트의 바디를 확인하고 싶다면 이렇게 하면 된다.
+		// BufferedReader br = request.getReader();
+		// String input = null;
+		// while((input = br.readLine()) != null) {
+		// 	System.out.println(input);
+		// }
+		User user = null;
+		try {
+			ObjectMapper om = new ObjectMapper(); //json 데이터 파싱해 줌
+			user = om.readValue(request.getInputStream(), User.class);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println(user);
+
+		UsernamePasswordAuthenticationToken authenticationToken =
+			new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+
+		//이게 실행될 때 PrincipalDetailsService의 loadUserByUsername()이 호출이 된다.
+		//authentication 에 내 로그인 한 정보가 담긴다.
+		System.out.println("authenticationToken = " + authenticationToken);
+		// Authentication authentication;
+		// try{
+			//로그인 시도를 위해서 authenticationManager로 로그인 시도를 하면 PrincipalDetailsService가 호출이 된다.
+			//그러면 PrincipalDetailsService에서 loadUserByUsername() 함수가 실행이 된다.
+			//이후 함수가 실행 후 정상이면 authentication 객체가 리턴된다.
+			//정상적인 로그인이라면 authentication 객체가 리턴된다.
+			//즉 db안의 username과 password가 일치한다는 뜻이다.
+		Authentication authentication = authenticationManager.authenticate(authenticationToken);
+		// }
+		// catch (AuthenticationException e){
+		// 	System.out.println("e = " + e);
+		// 	throw new RuntimeException(e);
+		// }
+		System.out.println("what?");
+		System.out.println("authentication = " + authentication.getPrincipal());
+		PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+		System.out.println(principalDetails.getUser().getUsername()); //로그인이 되었다는 것은 세션에 저장이 되었다는 것이다.
+		System.out.println("===================================");
+		//authentication 객체가 session 영역에 저장되었다 (리턴될때)
+		// 리턴해주는 이유는 권한 관리를 시큐리티가 대신 해주기 때문에 편하게 하려고 하는 것이다.
+		// 굳이 JWT 토큰을 사용하면서 세션을 만들 이유가 없지만 단지 권한 처리때문에 session에 넣어준다.
+
+		return authentication; //이게 리턴되면 실행되는 함수가 있다. successfulAuthentication()
+	}
+
+	//attemptAuthentication 실행 후 인증이 정상적으로 되었으면 successfulAuthentication 함수가 실행된다.
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+		Authentication authResult) throws IOException, ServletException {
+		System.out.println("successfulAuthentication 실행됨: 인증이 완료되었다는 뜻");
+		PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
 	}
 }
